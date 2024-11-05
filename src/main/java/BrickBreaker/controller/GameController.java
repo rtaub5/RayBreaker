566a1ed51f539c45c574e98f67e9add929a5909b
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-11-05 11:51:01 -0500

    game over

--- src/main/java/BrickBreaker/controller/GameController.java
+++ src/main/java/BrickBreaker/controller/GameController.java
@@ -103,6 +103,9 @@ public class GameController {
             //recalculate angle if it did intersect
             game.removeBrick(intersect);
             game.setBallAngle();
+            if(game.getBricks().isEmpty()) {
+                gameOver();
+            }
         }
         else if(intersect == -2) { //hit bottom wall
             gameOver();
