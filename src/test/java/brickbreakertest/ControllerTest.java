66f6b0be920813513c23f23378c58800831662b0
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-11-11 15:19:04 -0500

    added tests

--- src/test/java/brickbreakertest/ControllerTest.java
+++ src/test/java/brickbreakertest/ControllerTest.java
@@ -1,8 +1,41 @@
 package brickbreakertest;
 
+import brickbreaker.controller.GameController;
+import brickbreaker.model.Game;
+import brickbreaker.model.Paddle;
+import brickbreaker.view.GameComponent;
+import brickbreaker.view.GameFrame;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+
+import static org.mockito.Mockito.*;
+
 public class ControllerTest {
 
-    //test for gamecontroller.movePaddle()
+    @Test
+    public void movePaddle() {
+        GameComponent view = mock();
+        GameFrame model = mock();
+        Game game = mock();
+        Paddle paddle = mock();
 
-    //test for gamecontroller.moveBall()
+        doReturn(game).when(view).getGame();
+        doReturn(paddle).when(game).getPaddle();
+
+        GameController gameController = new GameController(model, view);
+
+        KeyEvent rightKeyEvent = new KeyEvent(new JButton(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, 'R');
+        gameController.movePaddle(rightKeyEvent);
+
+        verify(paddle).setDirection(true);
+
+        KeyEvent leftKeyEvent = new KeyEvent(new JButton(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, 'L');
+        gameController.movePaddle(leftKeyEvent);
+
+        verify(paddle).setDirection(false);
+
+    }
+
 }
